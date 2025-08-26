7cf67a0774602df3ec6fcee6aa730ee0f837ec46
Alberto Bastianello <alberto.bast2001@gmail.com>
2025-08-26 18:47:29 +0200

    hot fix in stats creation

--- storage/statsStorer.go
+++ storage/statsStorer.go
@@ -2,6 +2,7 @@ package storage
 
 import (
 	"errors"
+	"math/big"
 
 	"github.com/NaeuralEdgeProtocol/ratio1-backend/model"
 	"gorm.io/gorm"
@@ -13,16 +14,30 @@ func CreateStats(stats *model.Stats) error {
 		return err
 	}
 
-	txCreate := db.Create(&stats)
-	if txCreate.Error != nil {
-		txCreate.Rollback()
-		return txCreate.Error
-	}
-	if txCreate.RowsAffected == 0 {
-		txCreate.Rollback()
-		return gorm.ErrRecordNotFound
+	row := map[string]any{
+		"creation_timestamp":           stats.CreationTimestamp,
+		"daily_active_jobs":            stats.DailyActiveJobs,
+		"daily_usdc_locked":            toNumericExpr(stats.DailyUsdcLocked),
+		"daily_token_burn":             toNumericExpr(stats.DailyTokenBurn),
+		"total_token_burn":             toNumericExpr(stats.TotalTokenBurn),
+		"daily_nd_contract_token_burn": toNumericExpr(stats.DailyNdContractTokenBurn),
+		"total_nd_contract_token_burn": toNumericExpr(stats.TotalNdContractTokenBurn),
+		"daily_poai_rewards":           toNumericExpr(stats.DailyPOAIRewards),
+		"total_poai_rewards":           toNumericExpr(stats.TotalPOAIRewards),
+		"daily_minted":                 toNumericExpr(stats.DailyMinted),
+		"total_minted":                 toNumericExpr(stats.TotalMinted),
+		"total_supply":                 toNumericExpr(stats.TotalSupply),
+		"team_wallets_supply":          toNumericExpr(stats.TeamWalletsSupply),
+		"last_block_number":            stats.LastBlockNumber, // bigint
 	}
 
+	res := db.Table("stats").Create(row)
+	if res.Error != nil {
+		return res.Error
+	}
+	if res.RowsAffected != 1 {
+		return errors.New("no row inserted")
+	}
 	return nil
 }
 
@@ -62,4 +77,11 @@ func GetAllStatsASC() (*[]model.Stats, error) {
 	}
 
 	return &stats, nil
+}
+
+func toNumericExpr(x *big.Int) any {
+	if x == nil {
+		return nil // scrive NULL
+	}
+	return gorm.Expr("?::numeric", x.String())
 }
