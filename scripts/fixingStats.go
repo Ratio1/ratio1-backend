b844a6f55340bd24b2421864d5b16d0003daabde
Alberto Bastianello <alberto.bast2001@gmail.com>
2025-10-02 17:00:15 +0200

    fix: improved script

--- scripts/fixingStats.go
+++ scripts/fixingStats.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"math/big"
+	"time"
 )
 
 func main() {
@@ -17,31 +18,33 @@ func main() {
 	}
 
 	//get all dayly poai rewards ordered by block number
-	allDailyPoaiRewards := make(map[int64]*big.Int)
+	allDailyPoaiRewards := make(map[int]*big.Int)
 	for _, alloc := range allAllocations {
 		amount := alloc.GetUsdcAmountPayed()
-		if _, ok := allDailyPoaiRewards[alloc.BlockNumber]; !ok {
-			allDailyPoaiRewards[alloc.BlockNumber] = big.NewInt(0)
+		if _, ok := allDailyPoaiRewards[getEpoch(alloc.AllocationCreation)]; !ok {
+			allDailyPoaiRewards[getEpoch(alloc.AllocationCreation)] = big.NewInt(0)
 		}
-		allDailyPoaiRewards[alloc.BlockNumber].Add(allDailyPoaiRewards[alloc.BlockNumber], amount)
+		allDailyPoaiRewards[getEpoch(alloc.AllocationCreation)].Add(allDailyPoaiRewards[getEpoch(alloc.AllocationCreation)], amount)
 	}
+
 	//update all stats with daily poai rewards and total poai rewards
 	for k, v := range allDailyPoaiRewards {
 		for i, stat := range allStats {
-			if stat.LastBlockNumber >= k {
+			if getEpoch(stat.CreationTimestamp) == k {
 				stat.DailyPOAIRewards = v
 				if stat.TotalPOAIRewards == nil {
 					stat.TotalPOAIRewards = big.NewInt(0)
 				}
 				stat.TotalPOAIRewards.Add(allStats[i-1].TotalPOAIRewards, v)
 				allStats[i] = stat
+				break
 			}
 		}
 	}
 
 	// recalculate total daily usdc locked
 	for i, stat := range allStats {
-		allStats[i].DailyUsdcLocked = big.NewInt(0).Sub(stat.DailyUsdcLocked, stat.TotalPOAIRewards)
+		allStats[i].DailyUsdcLocked.Sub(stat.DailyUsdcLocked, stat.TotalPOAIRewards)
 	}
 
 	//save on storage
@@ -51,4 +54,9 @@ func main() {
 			panic(err)
 		}
 	}
+}
+
+func getEpoch(date time.Time) int {
+	mainnetStart := time.Unix(1748016000, 0)
+	return int(date.Sub(mainnetStart) / (24 * time.Hour))
 }
