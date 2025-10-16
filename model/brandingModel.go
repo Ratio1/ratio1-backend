16384a793eedf045365c25e11bed53fc06f6baa9
Bastia01 <118429268+Bastia01@users.noreply.github.com>
2025-10-16 14:50:04 +0200

    Branding (#35)
    * feat: added basic branding apis
    
    * fix: changed get all brands in get brands
    
    * fix: changed new branding endpoint to editBrand entpoint, now one endpoint creates+updates brand
    
    * feat: added new logo endpoints plus getPlatforms endpoint
    
    * fix: simplified GetPlatforms() utils
    
    * fix: standardized platforms name
    
    * fix: parsePlatform issue
    
    * fix: updated sdk

---
+++ model/brandingModel.go
@@ -0,0 +1,128 @@
+package model
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"io"
+
+	"github.com/NaeuralEdgeProtocol/ratio1-backend/config"
+	"github.com/Ratio1/ratio1_sdk_go/pkg/r1fs"
+)
+
+type Branding struct {
+	UserAddress string  `gorm:"type:varchar(66);primaryKey" json:"userAddress"`
+	Name        string  `gorm:"type:text"  json:"name"`
+	Description string  `gorm:"type:text"  json:"description"`
+	Links       string  `gorm:"type:jsonb;default:'{}'"  json:"links"`
+	CidLogo     *string `gorm:"type:text"  json:"cidLogo"`
+}
+
+func (b *Branding) GetLogoBase64() ([]byte, error) {
+	if b.CidLogo == nil {
+		return nil, errors.New("no cid found")
+	}
+	data, _, err := config.Config.R1fsClient.GetFileBase64(context.Background(), *b.CidLogo, "")
+	if err != nil {
+		return nil, errors.New("error while retrieving file from r1fs: " + err.Error())
+	}
+	return data, nil
+}
+
+func (b *Branding) SetLogoBase64(logoReader io.Reader, filename string) error {
+	cid, err := config.Config.R1fsClient.AddFileBase64(context.Background(), logoReader, &r1fs.DataOptions{Filename: filename})
+	if err != nil {
+		return errors.New("error while uploading file to r1fs: " + err.Error())
+	}
+	b.CidLogo = &cid
+	return nil
+}
+
+func (b *Branding) GetLinks() (map[string]string, error) {
+	platformsLinks := make(map[Platform]string)
+	err := json.Unmarshal([]byte(b.Links), &platformsLinks)
+	if err != nil {
+		return nil, err
+	}
+	result := make(map[string]string)
+	for k, v := range platformsLinks {
+		result[k.String()] = v
+	}
+	return result, nil
+}
+
+func (b *Branding) SetLinks(links string) error {
+	platformLinks := make(map[string]string)
+	err := json.Unmarshal([]byte(links), &platformLinks)
+	if err != nil {
+		return err
+	}
+
+	result := make(map[Platform]string)
+	for k, v := range platformLinks {
+		if !parsePlatform(k).isValid() {
+			return errors.New("unknown platform")
+		}
+		result[parsePlatform(k)] = v
+	}
+	resultBytes, err := json.Marshal(result)
+	if err != nil {
+		return err
+	}
+	b.Links = string(resultBytes)
+	return nil
+}
+
+const (
+	X        = "X"
+	Linkedin = "Linkedin"
+	Website  = "Website"
+)
+
+type Platform int
+
+const (
+	PlatformUnknown Platform = iota
+	PlatformX
+	PlatformLinkedin
+	PlatformWebsite
+)
+
+var platformNames = map[Platform]string{
+	PlatformX:        X,
+	PlatformLinkedin: Linkedin,
+	PlatformWebsite:  Website,
+}
+
+var platformValues = map[string]Platform{
+	X:        PlatformX,
+	Linkedin: PlatformLinkedin,
+	Website:  PlatformWebsite,
+}
+
+func (p Platform) String() string {
+	if name, ok := platformNames[p]; ok {
+		return name
+	}
+	return "Unknown"
+}
+
+func parsePlatform(s string) Platform {
+	if p, ok := platformValues[s]; ok {
+		return p
+	}
+	return PlatformUnknown
+}
+
+func (p Platform) isValid() bool {
+	_, ok := platformNames[p]
+	return ok
+}
+
+func (p Platform) GetPlatforms() []string {
+	var result []string
+	for _, v := range platformNames {
+		result = append(result, v)
+	}
+	return result
+}
---
+++ proxy/handlers/branding.go
@@ -0,0 +1,323 @@
+package handlers
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+	"path/filepath"
+	"strings"
+
+	"github.com/NaeuralEdgeProtocol/ratio1-backend/config"
+	"github.com/NaeuralEdgeProtocol/ratio1-backend/model"
+	"github.com/NaeuralEdgeProtocol/ratio1-backend/proxy/middleware"
+	"github.com/NaeuralEdgeProtocol/ratio1-backend/service"
+	"github.com/NaeuralEdgeProtocol/ratio1-backend/storage"
+	"github.com/gin-gonic/gin"
+)
+
+const (
+	baseBrandingEndpoint   = "/branding"
+	editBrandEndpoint      = "/edit"
+	editBrandLogoEndpoint  = "/edit-logo"
+	getBrandsEndpoints     = "/get-brands"
+	getBrandsLogosEndpoint = "/get-brand-logo"
+	getPlatformsEndpoint   = "/get-platforms"
+)
+
+type editBrandRequest struct {
+	Name        string            `json:"name"`
+	Description string            `json:"description"`
+	Links       map[string]string `json:"links"`
+}
+
+type getBrandsrequest struct {
+	Addresses []string `json:"brandAddresses"`
+}
+
+type getBrandsResponse struct {
+	Brands []ParsedBrands `json:"brands"`
+}
+
+type ParsedBrands struct {
+	UserAddress string            `json:"brandAddress"`
+	Name        string            `json:"name,omitempty"`
+	Description string            `json:"description,omitempty"`
+	Links       map[string]string `json:"links,omitempty"`
+}
+
+type brandingHandler struct{}
+
+func NewBrandingHandler(groupHandler *groupHandler) {
+	h := brandingHandler{}
+
+	//pub endpoiints
+	pubEndpoints := []EndpointHandler{
+		{Method: http.MethodPost, Path: getBrandsEndpoints, HandlerFunc: h.getBrands},
+		{Method: http.MethodGet, Path: getBrandsLogosEndpoint, HandlerFunc: h.getBrandLogo},
+		{Method: http.MethodGet, Path: getPlatformsEndpoint, HandlerFunc: h.getPlatforms},
+	}
+	pubEndpointGroupHandler := EndpointGroupHandler{
+		Root:             baseBrandingEndpoint,
+		Middleware:       []gin.HandlerFunc{},
+		EndpointHandlers: pubEndpoints,
+	}
+	groupHandler.AddEndpointGroupHandler(pubEndpointGroupHandler)
+
+	//auth endpooints
+	authEndpoints := []EndpointHandler{
+		{Method: http.MethodPost, Path: editBrandEndpoint, HandlerFunc: h.editBrand},
+		{Method: http.MethodPost, Path: editBrandLogoEndpoint, HandlerFunc: h.editBrandLogo},
+	}
+
+	auth := middleware.Authorization(config.Config.Jwt.Secret)
+	authEndpointGroupHandler := EndpointGroupHandler{
+		Root:             baseBrandingEndpoint,
+		Middleware:       []gin.HandlerFunc{auth},
+		EndpointHandlers: authEndpoints,
+	}
+	groupHandler.AddEndpointGroupHandler(authEndpointGroupHandler)
+}
+
+func (h *brandingHandler) editBrand(c *gin.Context) {
+	nodeAddress, err := service.GetAddress()
+	if err != nil {
+		log.Error("error while retrieving node address: " + err.Error())
+		model.JsonResponse(c, http.StatusInternalServerError, nil, "", err.Error())
+		return
+	}
+
+	userAddress, err := middleware.AddressFromBearer(c)
+	if err != nil {
+		log.Error("error while retrieving address from bearer: " + err.Error())
+		model.JsonResponse(c, http.StatusBadRequest, nil, nodeAddress, err.Error())
+		return
+	}
+
+	var req editBrandRequest
+	err = c.Bind(&req)
+	if err != nil {
+		err = errors.New("error while binding request: " + err.Error())
+		log.Error(err.Error())
+		model.JsonResponse(c, http.StatusBadRequest, nil, nodeAddress, err.Error())
+		return
+	}
+
+	brand := model.Branding{
+		UserAddress: userAddress,
+		Name:        req.Name,
+		Description: req.Description,
+	}
+
+	linksAsByte, err := json.Marshal(req.Links)
+	if err != nil {
+		err = errors.New("error while marshalling links: " + err.Error())
+		log.Error(err.Error())
+		model.JsonResponse(c, http.StatusBadRequest, nil, nodeAddress, err.Error())
+		return
+	}
+
+	err = brand.SetLinks(string(linksAsByte))
+	if err != nil {
+		err = errors.New("error while setting brand links: " + err.Error())
+		log.Error(err.Error())
+		model.JsonResponse(c, http.StatusBadRequest, nil, nodeAddress, err.Error())
+		return
+	}
+
+	err = storage.SaveBrand(&brand)
+	if err != nil {
+		err = errors.New("error while saving brand: " + err.Error())
+		log.Error(err.Error())
+		model.JsonResponse(c, http.StatusBadRequest, nil, nodeAddress, err.Error())
+		return
+	}
+
+	model.JsonResponse(c, http.StatusOK, nil, nodeAddress, "")
+}
+
+func (h *brandingHandler) editBrandLogo(c *gin.Context) {
+	nodeAddress, err := service.GetAddress()
+	if err != nil {
+		log.Error("error while retrieving node address: " + err.Error())
+		model.JsonResponse(c, http.StatusInternalServerError, nil, "", err.Error())
+		return
+	}
+
+	userAddress, err := middleware.AddressFromBearer(c)
+	if err != nil {
+		log.Error("error while retrieving address from bearer: " + err.Error())
+		model.JsonResponse(c, http.StatusBadRequest, nil, nodeAddress, err.Error())
+		return
+	}
+
+	//get file
+	file, err := c.FormFile("logo")
+	if err != nil {
+		err = errors.New("error while retrieving logo: " + err.Error())
+		log.Error(err.Error())
+		model.JsonResponse(c, http.StatusBadRequest, nil, nodeAddress, err.Error())
+		return
+	}
+
+	ext := strings.ToLower(filepath.Ext(file.Filename))
+	if ext == "" {
+		ext = ".jpg" // fallback
+	}
+	if !isAllowedExt(ext) {
+		err = errors.New("invalid extension")
+		log.Error(err.Error())
+		model.JsonResponse(c, http.StatusBadRequest, nil, nodeAddress, err.Error())
+		return
+	}
+
+	//get file reader
+	fileReader, err := file.Open()
+	if err != nil {
+		err = errors.New("error while opening logo: " + err.Error())
+		log.Error(err.Error())
+		model.JsonResponse(c, http.StatusBadRequest, nil, nodeAddress, err.Error())
+		return
+	}
+	defer fileReader.Close()
+
+	brand, err := storage.GetBrandByAddress(userAddress)
+	if err != nil {
+		err = errors.New("error while retrieving brand: " + err.Error())
+		log.Error(err.Error())
+		model.JsonResponse(c, http.StatusBadRequest, nil, nodeAddress, err.Error())
+		return
+	} else if brand == nil {
+		err = errors.New("error while retrieving brand: no brand found")
+		log.Error(err.Error())
+		model.JsonResponse(c, http.StatusBadRequest, nil, nodeAddress, err.Error())
+		return
+	}
+
+	err = brand.SetLogoBase64(fileReader, file.Filename)
+	if err != nil {
+		err = errors.New("error while setting brand logo: " + err.Error())
+		log.Error(err.Error())
+		model.JsonResponse(c, http.StatusBadRequest, nil, nodeAddress, err.Error())
+		return
+	}
+
+	err = storage.SaveBrand(brand)
+	if err != nil {
+		err = errors.New("error while saving brand: " + err.Error())
+		log.Error(err.Error())
+		model.JsonResponse(c, http.StatusBadRequest, nil, nodeAddress, err.Error())
+		return
+	}
+
+	model.JsonResponse(c, http.StatusOK, nil, nodeAddress, "")
+}
+
+func (h *brandingHandler) getBrands(c *gin.Context) {
+	nodeAddress, err := service.GetAddress()
+	if err != nil {
+		log.Error("error while retrieving node address: " + err.Error())
+		model.JsonResponse(c, http.StatusInternalServerError, nil, "", err.Error())
+		return
+	}
+
+	var req getBrandsrequest
+	err = c.Bind(&req)
+	if err != nil {
+		err = errors.New("error while binding request: " + err.Error())
+		log.Error(err.Error())
+		model.JsonResponse(c, http.StatusBadRequest, nil, nodeAddress, err.Error())
+		return
+	}
+
+	var response getBrandsResponse
+	for _, a := range req.Addresses {
+		b, err := storage.GetBrandByAddress(a)
+		if err != nil {
+			err = errors.New("error while retrieving brand: " + err.Error())
+			log.Error(err.Error())
+			model.JsonResponse(c, http.StatusBadRequest, nil, nodeAddress, err.Error())
+			return
+		}
+		if b == nil {
+			continue
+		}
+		links, err := b.GetLinks()
+		if err != nil {
+			err = errors.New("error while retrieving links for brand " + b.Name + " :" + err.Error())
+			log.Error(err.Error())
+			model.JsonResponse(c, http.StatusBadRequest, nil, "", err.Error())
+			return
+		}
+		p := ParsedBrands{
+			UserAddress: b.UserAddress,
+			Name:        b.Name,
+			Description: b.Description,
+			Links:       links,
+		}
+		response.Brands = append(response.Brands, p)
+	}
+
+	model.JsonResponse(c, http.StatusOK, response, nodeAddress, "")
+}
+
+func (h *brandingHandler) getBrandLogo(c *gin.Context) {
+	nodeAddress, err := service.GetAddress()
+	if err != nil {
+		log.Error("error while retrieving node address: " + err.Error())
+		model.JsonResponse(c, http.StatusInternalServerError, nil, "", err.Error())
+		return
+	}
+
+	address := c.Query("address")
+	if address == "" {
+		err = errors.New("no address provided")
+		log.Error(err.Error())
+		model.JsonResponse(c, http.StatusBadRequest, nil, nodeAddress, err.Error())
+		return
+	}
+
+	brand, err := storage.GetBrandByAddress(address)
+	if err != nil {
+		err = errors.New("error while retrieving brand: " + err.Error())
+		log.Error(err.Error())
+		model.JsonResponse(c, http.StatusBadRequest, nil, nodeAddress, err.Error())
+		return
+	}
+	if brand == nil {
+		err = errors.New("brand does not exist")
+		log.Error(err.Error())
+		model.JsonResponse(c, http.StatusBadRequest, nil, nodeAddress, err.Error())
+		return
+	}
+
+	logo, err := brand.GetLogoBase64()
+	if err != nil {
+		err = errors.New("error while retrieving logo from r1fs: " + err.Error())
+		log.Error(err.Error())
+		model.JsonResponse(c, http.StatusBadRequest, nil, nodeAddress, err.Error())
+		return
+	}
+
+	ct := http.DetectContentType(logo)
+	c.Data(http.StatusOK, ct, logo)
+}
+
+func (h *brandingHandler) getPlatforms(c *gin.Context) {
+	nodeAddress, err := service.GetAddress()
+	if err != nil {
+		log.Error("error while retrieving node address: " + err.Error())
+		model.JsonResponse(c, http.StatusInternalServerError, nil, "", err.Error())
+		return
+	}
+	response := model.Platform(0).GetPlatforms()
+	model.JsonResponse(c, http.StatusOK, response, nodeAddress, "")
+}
+
+func isAllowedExt(ext string) bool {
+	switch ext {
+	case ".jpg", ".jpeg", ".png":
+		return true
+	default:
+		return false
+	}
+}
