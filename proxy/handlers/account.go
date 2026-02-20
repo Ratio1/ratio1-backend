e029bbc08e20b55cee5ba886546b982a0ee1e345
Alberto Bastianello <alberto.bast2001@gmail.com>
2026-02-20 17:30:57 +0100

    fix: updated order

--- proxy/handlers/account.go
+++ proxy/handlers/account.go
@@ -521,13 +521,6 @@ func (h *accountHandler) blackListAccount(c *gin.Context) {
 
 	var accountDto *model.AccountDto
 	if account.Email != nil {
-		err = service.SendBlacklistedEmail(*account.Email)
-		if err != nil {
-			log.Error("error while sending blacklisted email: " + err.Error())
-			model.JsonResponse(c, http.StatusBadRequest, nil, nodeAddress, err.Error())
-			return
-		}
-
 		kyc, _, err := storage.GetKycByEmail(*account.Email)
 		if err != nil {
 			log.Error("error while retrieving kyc information from storage: " + err.Error())
@@ -555,6 +548,15 @@ func (h *accountHandler) blackListAccount(c *gin.Context) {
 		log.Error("error while updating account: " + err.Error())
 		model.JsonResponse(c, http.StatusInternalServerError, nil, nodeAddress, err.Error())
 		return
+	}
+
+	if account.Email != nil {
+		err = service.SendBlacklistedEmail(*account.Email)
+		if err != nil {
+			log.Error("error while sending blacklisted email: " + err.Error())
+			model.JsonResponse(c, http.StatusBadRequest, nil, nodeAddress, err.Error())
+			return
+		}
 	}
 
 	model.JsonResponse(c, http.StatusOK, accountDto, nodeAddress, "")
